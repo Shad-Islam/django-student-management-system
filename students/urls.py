7c76da0da40f22223d7799cc452aae97d67b3eb0
Shad-islam <m.shadislam01@gmail.com>
2025-03-16 00:07:12 +0600

    change url name

--- students/urls.py
+++ students/urls.py
@@ -3,5 +3,5 @@ from .views import student_list, home
 
 urlpatterns = [
     path('', home),
-    path('student-list', student_list, name='student-list'),
+    path('student_list', student_list, name='student_list'),
 ]
